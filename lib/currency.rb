6375a3448282475bbc490132047749774e650c89
Balasubramani <eswar.balasubramani@acsicorp.com>
2022-03-21 20:12:13 +0530

    modification in denominaton name and value

--- lib/currency.rb
+++ lib/currency.rb
@@ -1,12 +1,12 @@
 class Currency
-  def  money(amount)
+  def  money(amount, coins)
       raise "value must be greater than zero" unless amount > 0
-        coins = {quarter: 25, dime: 10, nickel: 5, penny: 1}
+        # coins = {quarter: 25, dime: 10, nickel: 5, penny: 1}
         hash = {}
 
         remaining_amount = amount
         coins.each do |key, value|
-          count,amt = currency_div(remaining_amount, value)
+          count,amt = remaining_amount.divmod(value)
           hash[key.to_sym] = count
           remaining_amount = amt
         end
@@ -14,12 +14,8 @@ class Currency
       rescue => error
         error.message
   end
-  def currency_div(amount, coin)
-    amount.divmod(coin)
-  end
+
 end
 
-value1 = Currency.new
-p "Enter the amount"
-value2 = gets.chomp
-p value1.money(value2.to_i) 
+p Currency.new.money(444, {quarter: 25, dime: 10, nickel: 5, penny: 1})
+ 
